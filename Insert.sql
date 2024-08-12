125dac3a41402a7cc57328d93bf70a7da3c5dc77
Tungtom2004 <114893594+Tungtom2004@users.noreply.github.com>
2024-08-12 22:45:16 +0700

    Update Insert.sql

---
+++ Insert.sql
@@ -0,0 +1,15 @@
+INSERT INTO TABLE_NAME   
+(column1,   
+column2,   
+column3, ……columnN)    
+VALUES (value1, value2, value3, ….. valueN);    
+
+INSERT INTO table  
+(column1, column2, ... )  
+DEFAULT VALUES;  
+
+INSERT INTO table_name  
+(column1, column2, ... )  
+SELECT expression1, expression2, ...  
+FROM source_table  
+[WHERE conditions];  
